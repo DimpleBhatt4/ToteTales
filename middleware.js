289b06a74a52f78153d8557d9b835541123401b2
DimpleBhatt4 <dimplebhatt002@gmail.com>
2025-07-01 18:03:08 +0530

    Modify middleware bug

--- middleware.js
+++ middleware.js
@@ -1,41 +1,40 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  // Get the current request path
+
+  
+  // current request path
   const path = request.nextUrl.pathname;
 
 
-  // Define public paths
+  // public paths
   const isPublicPath =
     path === '/' || path === '/login' || path === '/signup';
 
-  // Get the token from cookies
+  //token from cookies
   const token = request.cookies.get('token')?.value || '';
 
-  // Allow access to the '/' route unconditionally
+
   if (path === '/') {
-    console.log("Accessing the homepage without requiring authentication");
     return NextResponse.next();
   }
 
   // Allow access to other public paths, even if no token exists
   if (isPublicPath) {
-    console.log(`Accessing public path: ${path}`);
     return NextResponse.next();
   }
 
   // Redirect to login if accessing restricted paths without a token
   if (!token && !isPublicPath) {
-    console.log("Redirecting to '/login' since no token exists");
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  // Allow the request to proceed if authenticated
+  //request to proceed if authenticated
   console.log(`Authenticated access to: ${path}`);
   return NextResponse.next();
 }
 
-// Specify the routes for which this middleware applies
+//routes for which this middleware applies
 export const config = {
-  matcher: ['/', '/login', '/signup', '/verifyemail', '/profile'], // Include the routes to match
+  matcher: ['/', '/login', '/signup', '/verifyemail', '/profile', '/profile/:path*', '/cart', '/wishlist'], // Include the routes to match
 };
